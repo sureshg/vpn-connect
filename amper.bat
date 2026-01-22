a5f4030172ecd06883d1e1939428dad89c43c5ca
Suresh G <sureshg.g@gmail.com>
2026-01-22 09:39:48 -0800

    chore: kotlin and amper updates

--- amper.bat
+++ amper.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.10.0-dev-3601
+set amper_version=0.10.0-dev-3615
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=25513dd1903e8e60b58dca0490f9d7a27f09a750c9a2cdcc0afde909f0a71945
+set amper_sha256=9c3005bb998e8962716e5ebafdb6b06ff3ff02029b00040bece957e90c78d59b
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
