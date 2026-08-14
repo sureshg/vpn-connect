e9ab44d6ee1099aa2645ba55bf7854affa50ed64
Suresh G <sureshg.g@gmail.com>
2026-08-13 20:49:12 -0700

    chore: clean up the platforms

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4242
+set kotlin_cli_version=0.12.0-dev-4253
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=bd9522d3116a8f971f8fbc63f66742653b670db160470934df70e5d51fa1cefd
+set kotlin_cli_sha256=e7121ee19b1079597651956898cc580bf42ee6f52d4c5ded3b74d60b0827e6ab
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
