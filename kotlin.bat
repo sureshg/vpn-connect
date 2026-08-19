cf1e7b0727fbb5f8749691a263a291efdefc1463
Suresh G <sureshg.g@gmail.com>
2026-08-19 10:46:39 -0700

    chore: update kotlin-vipaccess to 0.4.0 and Kotlin CLI

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4253
+set kotlin_cli_version=0.12.0-dev-4280
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=e7121ee19b1079597651956898cc580bf42ee6f52d4c5ded3b74d60b0827e6ab
+set kotlin_cli_sha256=dc54807097e37205d05d84adb82d6c769cd1c87f6e05c024ca69e5a68d803d5f
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
